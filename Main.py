23c72fe1e2cfd80a2ce62a70bbd9c9ee2ffc0666
jeniferj-30 <109952344+jeniferj-30@users.noreply.github.com>
2022-09-12 15:30:41 +0530

    commit

--- Main.py
+++ Main.py
@@ -1,7 +1,24 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
-  # Write your code here
+def selectionSort(array, size):
+	
+	for ind in range(size):
+		min_index = ind
+
+		for j in range(ind + 1, size):
+			# select the minimum element in every iteration
+			if array[j] < array[min_index]:
+				min_index = j
+		# swapping the elements to sort the array
+		(array[ind], array[min_index]) = (array[min_index], array[ind])
+
+arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
+size = len(arr)
+selectionSort(arr, size)
+print('The array after sorting in Ascending Order by selection sort is:')
+print(arr)
+
 
 # Do not change the following code
 input_data = input()
